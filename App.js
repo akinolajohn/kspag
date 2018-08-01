9dc582fd791e46ef1283d648382d533e54fecf44
John Akinola <jjakines@gmail.com>
2018-08-01 02:26:21 +0000

    Native Base Added

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome John Akinola It workeed!!! 
+        <Text>Welcome John Akinola It worked!!! 
           
             </Text> 
             <Text> Welcom Again  </Text>
